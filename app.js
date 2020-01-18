1cea297be7d1682270b3370910506c9557fd2491
Caleb Cress <caleb.cress04@gmail.com>
2020-01-18 13:57:18 -0800

    Setup server to work off catlin network for testing and started deconstructing times.

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ let options = {
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.end("{\"Zachary Robinson\":\"1579382958904\",\"Eric Wang\":\"1579370720199\",\"Kristin Cohrs\":\"1579383585246\",\"Dylan Smith\":\"1579382956820\",\"Mick L.\":\"1579370244114\",\"Joshua Negreanu\":\"1579381689703\",\"Liam Wang\":\"1579382952976\",\"Tiffany Toh\":\"1579382962426\",\"Caleb Cress\":\"1579380072660\"}")
     rp(options)
         .then(function (repos) {
             res.end(JSON.stringify(repos));
--- src/script.js
+++ src/script.js
@@ -2,8 +2,11 @@
 const request = new XMLHttpRequest();
 let data;
 
+let timeIn = new Date(1579370720199);
+console.log(((timeIn.getHours() - 1) % 12) + 1 + ':' + (timeIn.getMinutes() < 10 ? '0' : '') + timeIn.getMinutes());
+
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://team1540.catlin.edu:8443', true);
+request.open('GET', 'http://127.0.0.1:3000', true);
 
 request.onload = function () {
     console.log(this.response);
