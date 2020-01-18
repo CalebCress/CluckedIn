3d2273e580e5e28af3d8c904d78ab738618a8606
Caleb Cress <caleb.cress04@gmail.com>
2020-01-17 20:22:48 -0800

    Added backend node to get backend data from api

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const http = require('http');
+const rp = require('request-promise');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
+let options = {
+    uri: 'https://team1540.catlin.edu:8443/timesheet/loggedin',
+    headers: {
+        'User-Agent': 'Request-Promise'
+    },
+    auth: {
+        'user': 'cluckedin',
+        'pass': 'beanmachine',
+        'sendImmediately': false
+    },
+    json: true // Automatically parses the JSON string in the response
+};
+
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    rp(options)
+        .then(function (repos) {
+            res.end(JSON.stringify(repos));
+        })
+        .catch(function (err) {
+            // API call failed...
+            res.end('API Request Failed ' + err);
+        });
 });
 
 server.listen(port, hostname, () => {
